0324701134c2bb355860a3dd94d71be3e18de060
Yu Zhang <mzhangyjoy@gmail.com>
2023-03-06 19:47:50 -0600

    finish lab 01

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -6,14 +6,39 @@ There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
-    /* TODO: implement num_occurrences */
-    return 0;
+    int count = 0;
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == letter) {
+            count++;
+        }
+    }
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
-    /* TODO: implement compute_nucleotide_occurrences */
+    dna_seq->A_count = 0;
+    dna_seq->C_count = 0;
+    dna_seq->G_count = 0;
+    dna_seq->T_count = 0;
+    for (int i = 0; i < strlen(dna_seq->sequence); i++) {
+        char ch = dna_seq->sequence[i];
+        switch(ch) {
+            case 'A':
+                dna_seq->A_count++;
+                break;
+            case 'C':
+                dna_seq->C_count++;
+                break;
+            case 'G':
+                dna_seq->G_count++;
+                break;
+            case 'T':
+                dna_seq->T_count++;
+                break;
+        }
+    }
     return;
 }
--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,22 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    node dummyHead;
+    dummyHead.value = 0;
+    dummyHead.next = head;
+    node *slow_ptr = &dummyHead, *fast_ptr = &dummyHead;
+    if (fast_ptr->next == 0 || fast_ptr->next->next == 0) {
+        return 0;
+    } else {
+        fast_ptr = fast_ptr->next->next;
+    }
+    while (slow_ptr != fast_ptr) {
+        if (fast_ptr->next == 0 || fast_ptr->next->next == 0) {
+            return 0;
+        } else {
+            fast_ptr = fast_ptr->next->next;
+        }
+        slow_ptr = slow_ptr->next;
+    }
+    return 1;
 }
